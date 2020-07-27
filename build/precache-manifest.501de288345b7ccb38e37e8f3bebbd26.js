f37db19ff47ca9561e71f51bb9af056ba56ba6fb
Nicolas Verger <nicolas.india@gmail.com>
2020-07-27 22:44:06 +0200

    uibuild

---
+++ build/precache-manifest.501de288345b7ccb38e37e8f3bebbd26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9317965d2c75ab4c62e0d8e7b50fbd3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a137ead7d7467f2a845c",
+    "url": "/static/css/main.e51ea235.chunk.css"
+  },
+  {
+    "revision": "61f1ecaf5819353c2b59",
+    "url": "/static/js/2.c37eeb0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c37eeb0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a137ead7d7467f2a845c",
+    "url": "/static/js/main.cbd7c1e4.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let persons = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
